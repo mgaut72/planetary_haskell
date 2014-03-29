a8d0200b479036f6b108b9a8e771f0d4086edd34
Matt Gautreau <mgaut72@gmail.com>
2014-03-29 13:54:23 -0700

    add main to Main.hs to be able to compile

--- Main.hs
+++ Main.hs
@@ -4,8 +4,9 @@ import SolarSystem.Evolve
 import SolarSystem.Create (planets)
 import Control.Monad.Writer
 
---main = do
---  out <- runSystem
---  putStrLn $ snd $ runWriter out
+main = do
+  (_, log) <- return $ runWriter runSystem
+  putStr $ unlines $ log
+
 
 runSystem = evolveSystem 1 0.0 globalDt planets
